06549b3beb06282c35b6eb25ee98ee6b765d1ec4
leodamac <105889369+leodamac@users.noreply.github.com>
2023-06-19 19:33:57 -0500

    Creacion del proyecto
    Creacion del proyecto

---
+++ ProyectoPOO/src/main/java/com/mycompany/proyectopoo/ProyectoPOO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectopoo;
+
+/**
+ *
+ * @author Leo
+ */
+public class ProyectoPOO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
