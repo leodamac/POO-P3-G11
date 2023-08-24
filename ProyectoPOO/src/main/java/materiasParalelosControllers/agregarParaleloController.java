33416b5c067b71c656f57de76569adf8699ed27b
AngeloAEP <137355831+AngeloAEP@users.noreply.github.com>
2023-08-24 03:32:24 -0500

    cargar archivo paralelo
    se cagrga dos archivos csv con el listado de los estudiantes con su respectiva matricula, nombre y email

--- ProyectoPOO/src/main/java/materiasParalelosControllers/agregarParaleloController.java
+++ ProyectoPOO/src/main/java/materiasParalelosControllers/agregarParaleloController.java
@@ -1,8 +1,17 @@
 package materiasParalelosControllers;
 
 import academico.Materia;
+import academico.TerminoAcademico;
+import personas.Estudiante;
 import com.mycompany.poop3g11.App;
+import com.mycompany.poop3g11.Utilitario;
+import com.mycompany.proyectopoo.Menu;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import javafx.collections.FXCollections;
@@ -19,7 +28,7 @@ import javafx.stage.Stage;
 public class agregarParaleloController {
     private Materia materia;
     @FXML ComboBox<Materia> seleccionarMateriaComboBox;
-    @FXML TextField rutaArchivotextField;
+    @FXML TextField numeroParalelotextField, rutaArchivotextField;
 
     @FXML
     private void initialize(){
@@ -41,7 +50,7 @@ public class agregarParaleloController {
         FileChooser fileChooser = new FileChooser();
 
         // Agregar un filtro para solo mostrar archivos de texto
-        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Archivos de texto", "*.txt"));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Archivos de texto", "*.csv"));
 
         // Mostrar el cuadro de diálogo y obtener el archivo seleccionado
         File file = fileChooser.showOpenDialog(s);
@@ -49,6 +58,7 @@ public class agregarParaleloController {
         // Verificar si se seleccionó un archivo
         if (file != null) {
             rutaArchivotextField.setText(file.toString());
+            //cargarArchivoEstudiantes(); // Llama a la función para cargar estudiantes 
         }
     }
     
@@ -57,4 +67,57 @@ public class agregarParaleloController {
         ObservableList<Materia> observableList = FXCollections.observableArrayList(materias);
         seleccionarMateriaComboBox.setItems(observableList);
     }
+    @FXML
+    private void agregarButtonAccion(Event event){
+        String ruta = rutaArchivotextField.getText();
+        materia = seleccionarMateriaComboBox.getValue();
+        String cod = "";
+        ArrayList<Materia> materias = App.getTerminoAcademico().getMaterias();
+        for (Materia m : materias){
+            if (m.getNombre().equals(materia)){
+                cod = m.getCodigo();
+                break;
+            }
+        }
+        //verifica si el campo el campo Paralelo es un numero
+        if(!Utilitario.isNumero(numeroParalelotextField.getText())){
+            //muestra si el pop up es error
+            Utilitario.mostrarPopUp("¡ERROR!\nIngrese solo números\nen el CAMPO DE PARALELO", event);
+        }else{
+            
+            cargarArchivoEstudiantes(ruta,materia,cod);
+            Utilitario.mostrarPopUp("Paralelo registrado con ÉXITO", event);
+        }
+    }
+    @FXML
+    private void cargarArchivoEstudiantes(String rutaArchivo, Materia materia, String cod){
+        //String rutaArchivo = rutaArchivotextField.getText();
+        int paralelo = Integer.parseInt(numeroParalelotextField.getText());
+        if (!rutaArchivo.isEmpty() && materia != null && paralelo>0){
+            String rutaGuardar = "archivos/"+App.getTerminoAcademico().toString()+"/materias/"+cod;
+            String carpetaNewParalelo = numeroParalelotextField.getText()+".txt";
+            String rutaFinal = rutaGuardar+"/"+carpetaNewParalelo;
+            File carpetaMateria = new File(rutaFinal);
+            
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(rutaFinal))){
+    
+                try (BufferedReader br = new BufferedReader(new FileReader(rutaArchivo))){
+                    String linea;
+                    while ((linea = br.readLine()) != null){
+                        String[] datos = linea.split(",");
+                   // suponiendo que el archivo tiene el formato adecuado (MATRICULA, NOMBRE_ESTUDIANTE, Email)
+                        if (datos.length==3){
+                            writer.write(datos[0]+"\t"+ datos[1]+"\t"+datos[2]);
+                            writer.newLine();
+                        }
+                    }                      
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+    
 }
