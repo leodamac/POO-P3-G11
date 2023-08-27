e47cea8ba257e630ea41bddb623197e77d6ed755
AngeloAEP <137355831+AngeloAEP@users.noreply.github.com>
2023-08-27 17:55:42 -0500

    agregar paralelo
    correccion de agregar paralelo por medio del boton agregar

--- ProyectoPOO/src/main/java/materiasParalelosControllers/agregarParaleloController.java
+++ ProyectoPOO/src/main/java/materiasParalelosControllers/agregarParaleloController.java
@@ -19,6 +19,7 @@ import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
@@ -29,13 +30,16 @@ public class agregarParaleloController {
     private Materia materia;
     @FXML ComboBox<Materia> seleccionarMateriaComboBox;
     @FXML TextField numeroParalelotextField, rutaArchivotextField;
+    @FXML Button agregarButton;
 
     @FXML
     private void initialize(){
         cargarMaterias();
         seleccionarMateriaComboBox.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             materia = seleccionarMateriaComboBox.getValue();
+            
         });
+        agregarButton.setOnAction(ev->{agregarButtonAccion(ev);});    
     }
     // cambia a la ventana administrarTerminosAcademicos
     @FXML
@@ -58,7 +62,7 @@ public class agregarParaleloController {
         // Verificar si se seleccionó un archivo
         if (file != null) {
             rutaArchivotextField.setText(file.toString());
-            //cargarArchivoEstudiantes(); // Llama a la función para cargar estudiantes 
+            //cargarArchivoEstudiantes(file.getPath(), materia); // Llama a la función para cargar estudiantes 
         }
     }
     
@@ -71,37 +75,31 @@ public class agregarParaleloController {
     private void agregarButtonAccion(Event event){
         String ruta = rutaArchivotextField.getText();
         materia = seleccionarMateriaComboBox.getValue();
-        String cod = "";
-        ArrayList<Materia> materias = App.getTerminoAcademico().getMaterias();
-        for (Materia m : materias){
-            if (m.getNombre().equals(materia)){
-                cod = m.getCodigo();
-                break;
-            }
-        }
+       
+        
         //verifica si el campo el campo Paralelo es un numero
         if(!Utilitario.isNumero(numeroParalelotextField.getText())){
             //muestra si el pop up es error
             Utilitario.mostrarPopUp("¡ERROR!\nIngrese solo números\nen el CAMPO DE PARALELO", event);
         }else{
             
-            cargarArchivoEstudiantes(ruta,materia,cod);
+            cargarArchivoEstudiantes();
             Utilitario.mostrarPopUp("Paralelo registrado con ÉXITO", event);
         }
     }
     @FXML
-    private void cargarArchivoEstudiantes(String rutaArchivo, Materia materia, String cod){
+    private void cargarArchivoEstudiantes(){
         //String rutaArchivo = rutaArchivotextField.getText();
         int paralelo = Integer.parseInt(numeroParalelotextField.getText());
-        if (!rutaArchivo.isEmpty() && materia != null && paralelo>0){
-            String rutaGuardar = "archivos/"+App.getTerminoAcademico().toString()+"/materias/"+cod;
+        if (seleccionarMateriaComboBox.getValue()!= null && paralelo > 0){
+            String rutaGuardar = "archivos/"+App.getTerminoAcademico().toString()+"/materias/"+materia.getCodigo();
             String carpetaNewParalelo = numeroParalelotextField.getText()+".txt";
             String rutaFinal = rutaGuardar+"/"+carpetaNewParalelo;
             File carpetaMateria = new File(rutaFinal);
-            
+            System.out.println(rutaFinal);
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(rutaFinal))){
     
-                try (BufferedReader br = new BufferedReader(new FileReader(rutaArchivo))){
+                try (BufferedReader br = new BufferedReader(new FileReader(rutaArchivotextField.getText()))){
                     String linea;
                     while ((linea = br.readLine()) != null){
                         String[] datos = linea.split(",");
